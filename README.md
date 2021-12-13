82675f1edabc6b52fdd8b67e7ec24fd700cfeb05
absolutty <ada.hajro@gmail.com>
2021-12-14 00:01:29 +0100

    Update of README.md Upravenie zobrazovania informácií o aplikácií.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Telefónny zoznam
-Vytvoril Adam Hajro
+Vytvoril Adam Hajro <br>
+
+&-
+
+Aplikácia TelefonnyZoznam slúži na zobrazovanie, ukladanie a následné upravovanie
+telefónnych kontaktov. Uložené kontakty sa nachádzajú v .xml databáze. 
+
+Kontakt obsahuje atribúty: 
+- povinné (krstneMeno, priezvisko) 
+- nepovinné (telCislo, poznamka)
+
+-&
+
+![](https://github.com/absolutty/javafx.TelefonnyZoznam/blob/master/img-uk%C3%A1%C5%BEka/obr-1.PNG)
+![](https://github.com/absolutty/javafx.TelefonnyZoznam/blob/master/img-uk%C3%A1%C5%BEka/obr-2.PNG)
+![](https://github.com/absolutty/javafx.TelefonnyZoznam/blob/master/img-uk%C3%A1%C5%BEka/obr-3.PNG)
+![](https://github.com/absolutty/javafx.TelefonnyZoznam/blob/master/img-uk%C3%A1%C5%BEka/obr-4.PNG)
+![](https://github.com/absolutty/javafx.TelefonnyZoznam/blob/master/img-uk%C3%A1%C5%BEka/obr-5.PNG)
--- src/main/java/sk/uniza/fri/telefonnyzoznam/controllers/InfoController.java
+++ src/main/java/sk/uniza/fri/telefonnyzoznam/controllers/InfoController.java
@@ -2,15 +2,64 @@ package sk.uniza.fri.telefonnyzoznam.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.text.Font;
 import sk.uniza.fri.telefonnyzoznam.Main;
 
-public class InfoController {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class InfoController {
     @FXML public Label nazovAplikacie;
+    @FXML public TextArea vypisovaneInfo;
 
     public void initialize() {
         this.nazovAplikacie.setText(Main.NAZOV_APPLIKACIE);
         this.nazovAplikacie.setFont(new Font("Arial", 25));
+        this.vypisovaneInfo.setFont(new Font("Arial", 12));
+
+        this.vypisovaneInfo.setText(nacitajText());
+    }
+
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+    private static final String zaciatok_ODDELENIE = "&-";
+    private static final String koniec_ODDELENIE = "-&";
+    /**
+     * Táto metóda slúži načítanie README.md a vrátenie len toho textu, kt. sa nachádza medzi
+     * znakmi zaciatok_ODDELENIE "&-" a koniec_ODDELENIE "-&"
+     * Napríklad:
+     *
+     * nejaky text bla bla bla <br>
+     *
+     * &-
+     * Tento text bude zobrazený v informáciach o aplikácií
+     * -&
+     *
+     * @return String vytvoreného textu, môže sa priamo zobraziť v vypisovaneInfo
+     */
+    private static String nacitajText() {
+        boolean citajText = false;
+        File file = new File("README.md");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            StringBuilder contents = new StringBuilder();
+            String text;
+
+            while ((text = reader.readLine()) != null) {
+                if (text.equals(zaciatok_ODDELENIE)) { //najdeny ZACIATOK citania textu
+                    citajText = true;
+                } else if (text.equals(koniec_ODDELENIE)) { //najdeny KONIEC citania textu
+                    break;
+                } else {
+                    if (citajText) {
+                        contents.append(text).append(LINE_SEPARATOR);
+                    }
+                }
+            } return contents.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
